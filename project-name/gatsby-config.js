859068822cd626cb3b42f80cdfae3fa039095a5c
Simon Yost <simonyost@gmail.com>
2020-07-16 13:27:30 -0500

    started customizing

--- project-name/gatsby-config.js
+++ project-name/gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Simon Yost - Technology Leader`,
   },
   plugins: [
     {
@@ -26,9 +26,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Simon Yost`,
+        short_name: `Simon`,
+        description: `Technology Leader`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
